ebba3beb6edd80fdef350eb94467ab7a7b85f537
wuci <wuci@zhuojianchina.com>
2018-08-24 15:08:58 +0800

    新增lts

--- Myblog/src/main/java/com/myblog/MyblogApplication.java
+++ Myblog/src/main/java/com/myblog/MyblogApplication.java
@@ -1,8 +1,11 @@
 package com.myblog;
 
+import com.github.ltsopensource.spring.boot.annotation.EnableJobClient;
+import com.github.ltsopensource.spring.boot.annotation.EnableTaskTracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tk.mybatis.spring.annotation.MapperScan;
@@ -10,6 +13,8 @@ import tk.mybatis.spring.annotation.MapperScan;
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @ServletComponentScan
 @EnableTransactionManagement
+@EnableTaskTracker
+@EnableJobClient
 public class MyblogApplication {
 
 	public static void main(String[] args) {
